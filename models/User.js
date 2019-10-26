65db482fb90d22dae034f927e0d753d11a8af2ad
Carlos Alba <soradev4@gmail.com>
2019-10-26 16:38:32 -0700

    model update

--- models/User.js
+++ models/User.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
+    email: {
+        type: String, 
+        required: true, 
+        unique: true
+    }, 
+    school: {
+        type: String, 
+        required: true
+    }, 
     password: {
         type: String,
         required: true
