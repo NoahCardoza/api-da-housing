34ccfc212ee2fae3aa8d992561ca3d91e338cc0f
Carlos Alba <soradev4@gmail.com>
2019-10-30 09:04:34 -0700

    model

--- middleware/index.js
+++ middleware/index.js
@@ -2,7 +2,7 @@ const ListingModel = require("../models/Listing");
 
 module.exports.checkListingOwnership = (req, res, next) => {
 	if (!req.isAuthenticated()) return res.status(401);
-	Listing.findById(req.params.id, (err, doc) => {
+	ListingModel.findById(req.params.id, (err, doc) => {
 		if (err) return res.status(500);
 		if (!doc) return res.status(400);
 		if (doc.author.id === req.user._id) return next();
