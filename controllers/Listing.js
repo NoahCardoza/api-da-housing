4f40e50e200efd937cedae6fb421d2b5746ba09f
Carlos Alba <soradev4@gmail.com>
2019-10-30 09:06:40 -0700

    get listing

--- controllers/Listing.js
+++ controllers/Listing.js
@@ -1,35 +1,38 @@
-const express = require("express"); 
+const express = require("express");
 const router = express.Router();
 const ListingModel = require("../models/Listing");
 const middleware = require("../middleware");
 
 // INDEX route - show all listings
-router.get("/listing", function(req, res){
-	//get Listing from DataBase
-	Listing.find({},function(err, allListings){
-		if(err){
-			console.log(err);
-		} else {
-			res.send("/"); // {listings:allListings, currentUser: req.user});
-		}
-	});
+router.get("/listing", function (req, res) {
+	try {
+		return res.status(200).json(await ListingModel.find({}));
+	} catch (error) {
+		console.error(error);
+		return res.status(500);
+	}
 });
 
 // CREATE route
-router.post("/listing", middleware.isLoggedIn, function(req, res)
-{
+router.post("/listing", middleware.isLoggedIn, function (req, res) {
 	//get data from form and add to listing array
 	var name = req.body.name;
 	var image = req.body.image;
 	var price = req.body.price;
 	var desc = req.body.description;
 	var author = {
-		  id: req.user._id,
-          username: req.user.username
+		id: req.user._id,
+		username: req.user.username
 	}
-	var newListing = {name: name, price: price, image: image, description: desc, author:author}
-	Listing.create(newListing, function(err, newlyCreated){
-		if(err){
+	var newListing = {
+		name: name,
+		price: price,
+		image: image,
+		description: desc,
+		author: author
+	}
+	Listing.create(newListing, function (err, newlyCreated) {
+		if (err) {
 			console.log(err);
 		} else {
 			//reditect back to camground page
@@ -40,50 +43,50 @@ router.post("/listing", middleware.isLoggedIn, function(req, res)
 
 
 // SHOW route - presents info
-router.get("/:id", function(req, res){
-    //find the listing with provided ID
-	Listing.findById(req.params.id, function(err, foundListing){
-        if(err){
-            console.log(err);
-        } else {
+router.get("/:id", function (req, res) {
+	//find the listing with provided ID
+	Listing.findById(req.params.id, function (err, foundListing) {
+		if (err) {
+			console.log(err);
+		} else {
 			if (!foundListing) {
-            	return res.status(400).send("Item not found.")
-       		}
-            console.log(foundListing);
-            //render show template with that listing
-            res.send("listings/show") //, {listing: foundListing});
-        }
-    });
+				return res.status(400).send("Item not found.")
+			}
+			console.log(foundListing);
+			//render show template with that listing
+			res.send("listings/show") //, {listing: foundListing});
+		}
+	});
 });
 
 // EDIT listing route
-router.get("/:id/edit", middleware.checkListingOwnership, function(req, res){
-	Listing.findById(req.params.id, function(err, foundListing){
+router.get("/:id/edit", middleware.checkListingOwnership, function (req, res) {
+	Listing.findById(req.params.id, function (err, foundListing) {
 		if (!foundListing) {
-            	return res.status(400).send("Item not found.")
-       	}
+			return res.status(400).send("Item not found.")
+		}
 		res.send("listings/edit") // {listing: foundListing});
 	});
 });
-	
+
 // UPDATE listing route
-router.put("/:id", middleware.checkListingOwnership, function(req, res){
-	Listing.findByIdAndUpdate(req.params.id, req.body.listing, function(err, updateListing){
-		if(err){
+router.put("/:id", middleware.checkListingOwnership, function (req, res) {
+	Listing.findByIdAndUpdate(req.params.id, req.body.listing, function (err, updateListing) {
+		if (err) {
 			res.redirect("/listings");
 		} else {
 			if (!updateListing) {
-            	return res.status(400).send("Item not found.")
-       		}
+				return res.status(400).send("Item not found.")
+			}
 			res.redirect("/listings/" + req.params.id);
 		}
 	})
 })
 
 // DESTROY listing route
-router.delete("/:id", middleware.checkListingOwnership, function(req, res) {
-	Listing.findByIdAndRemove(req.params.id, function(err){
-		if(err){
+router.delete("/:id", middleware.checkListingOwnership, function (req, res) {
+	Listing.findByIdAndRemove(req.params.id, function (err) {
+		if (err) {
 			res.redirect("/listings");
 		} else {
 			res.redirect("/listings");
