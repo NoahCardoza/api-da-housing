321e204f5e374881d0967540a06d81bbdffa460a
Carlos Alba <soradev4@gmail.com>
2019-10-30 09:22:13 -0700

    semi colon

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
-})
+});
 
 // Routes
 const UserRouter = require('./controllers/User');
