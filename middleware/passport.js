0dfd4634ef664e1fcb9578e4ed8556fdf02f00b6
Carlos Alba <soradev4@gmail.com>
2019-10-30 13:16:11 -0700

    change

--- middleware/passport.js
+++
@@ -1,9 +0,0 @@
-const { Strategy, ExtractJwt } = require('passport-jwt'); 
-const { userModel } = require('../models/User');
-
-module.exports = (passport) => {
-    passport.use(new Strategy({
-        secretOrKey: process.env.SECRET, 
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
-    }), )
-}
