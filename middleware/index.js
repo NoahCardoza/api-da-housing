6ca97f64faede7e7277f9fb6d080640017baa083
Carlos Alba <soradev4@gmail.com>
2019-10-30 09:04:24 -0700

    Model

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,4 @@
-const Listing = require("../models/Listing");
+const ListingModel = require("../models/Listing");
 
 module.exports.checkListingOwnership = (req, res, next) => {
 	if (!req.isAuthenticated()) return res.status(401);
