7aedbe38288c29c26e4b2862bf0284bb30a9ab9b
Carlos Alba <soradev4@gmail.com>
2019-10-30 16:09:58 -0700

    auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,22 @@
 const jwt = require('jsonwebtoken');
-const { userModel } = require('../models/User');
+const {
+    userModel
+} = require('../models/User');
 
-module.exports.auth = async(req, res, next) => {
-    
-}   
+module.exports.auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const data = jwt.verify(token, process.env.SECRET);
+        const user = await userModel.findOne({
+            _id: data._id,
+            'tokens.token': token
+        });
+        if (!user) throw new Error('Auth failed!');
+        req.user = user;
+        req.token = token;
+        return next();
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Something went wrong with the Auth layerer!!');
+    }
+}
