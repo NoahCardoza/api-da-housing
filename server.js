d5b5b1c59e37a0e2fa0cfb994c6239ce2b02237a
Carlos Alba <soradev4@gmail.com>
2019-10-30 10:34:54 -0700

    upgrade mongoose

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
-	useUnifiedTopology: true
+	useUnifiedTopology: true, 
+	useCreateIndex: true
 });
 
 // Routes
