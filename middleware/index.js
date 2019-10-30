980f3b715ec252ae41531f9f320525792ae6ee77
Carlos Alba <soradev4@gmail.com>
2019-10-30 09:04:08 -0700

    refactor

--- middleware/index.js
+++ middleware/index.js
@@ -1,30 +1,12 @@
 var Listing = require("../models/Listing");
 
-module.exports.checkListingOwnership = function (req, res, next) {
-	if (req.isAuthenticated()) {
-		Listing.findById(req.params.id, function (err, foundListing) {
-			if (err) {
-				res.redirect("back");
-			} else {
-				if (!foundListing) {
-					return res.status(400).send("Item not found.")
-				}
-				if (foundListing.author.id.equals(req.user._id)) {
-					next();
-				} else {
-					res.redirect("back");
-				}
-			}
-		});
-	} else {
-		res.redirect("back");
-	}
-}
-
 module.exports.checkListingOwnership = (req, res, next) => {
-	if(!req.isAuthenticated()) return res.status(401); 
+	if (!req.isAuthenticated()) return res.status(401);
 	Listing.findById(req.params.id, (err, doc) => {
-		c
+		if (err) return res.status(500);
+		if (!doc) return res.status(400);
+		if (doc.author.id === req.user._id) return next();
+		return res.status(400);
 	});
 }
 
