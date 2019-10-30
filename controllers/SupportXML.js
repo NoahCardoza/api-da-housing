4dca6c09284e0fe844909271afb8fc1c6e514905
Carlos Alba <soradev4@gmail.com>
2019-10-30 16:51:09 -0700

    change

--- controllers/SupportXML.js
+++ controllers/SupportXML.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const xml = require('xml');
 const ListingModel = require('../models/Listing');
 
-router.get('/xml/listings', async (req, res) => {
+router.get('/xml/listings', async (_, res) => {
     try {
         res.set('Content-Type', 'text/xml');
         res.status(200).send(xml(await ListingModel.find({})))
