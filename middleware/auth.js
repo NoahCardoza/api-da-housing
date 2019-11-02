c06e92fb97138ec9e42dfd0bfc30017601bc0e3c
Carlos Alba <soradev4@gmail.com>
2019-11-02 14:21:44 -0700

    fmt

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,43 +1,43 @@
 const jwt = require('jsonwebtoken');
 const {
-    userModel
+  userModel,
 } = require('../models/User');
 const ListingModel = require('../models/Listing');
 
 
 module.exports.auth = async (req, res, next) => {
-    try {
-        const token = req.header('Authorization').replace('Bearer ', '');
-        const data = jwt.verify(token, process.env.SECRET);
-        const user = await userModel.findOne({
-            _id: data._id,
-            'tokens.token': token
-        }).exec();
-        if (!user) throw new Error('Credentials failed.');
-        req.user = user;
-        req.token = token;
-        return next();
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Your credentials have failed the auth layer.');
-    }
-}
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const data = jwt.verify(token, process.env.SECRET);
+    const user = await userModel.findOne({
+      _id: data._id,
+      'tokens.token': token,
+    }).exec();
+    if (!user) throw new Error('Credentials failed.');
+    req.user = user;
+    req.token = token;
+    return next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Your credentials have failed the auth layer.');
+  }
+};
 
 module.exports.isListingOwner = async (req, res, next) => {
-    try {
-        const token = req.header('Authorization').replace('Bearer ', '');
-        const data = jwt.verify(token, process.env.SECRET);
-        const listingID = req.params.listingid;
-        const listing = await ListingModel.findOne({
-            _id: listingID,
-            author: data._id
-        }).exec();
-        if (!listing) throw new Error('Credentials failed.');
-        req.listing = listing;
-        req.token = token;
-        return next();
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Your credentials have failed the auth layer.');
-    }
-}
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const data = jwt.verify(token, process.env.SECRET);
+    const listingID = req.params.listingid;
+    const listing = await ListingModel.findOne({
+      _id: listingID,
+      author: data._id,
+    }).exec();
+    if (!listing) throw new Error('Credentials failed.');
+    req.listing = listing;
+    req.token = token;
+    return next();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Your credentials have failed the auth layer.');
+  }
+};
