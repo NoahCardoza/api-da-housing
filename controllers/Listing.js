4ea019f381a446cb38d98601ed03110c99b7d79a
Carlos Alba <soradev4@gmail.com>
2019-11-02 11:27:53 -0700

    Listing not found

--- controllers/Listing.js
+++ controllers/Listing.js
@@ -19,8 +19,9 @@ router.get("/listing", async (_, res) => {
 // READ LISTING BY ID
 router.get("/get-listing/:listingid", async(req, res) => {
 	try{
-		const listing = ListingModel.findById(req.params.listingid);
-		
+		const listing = await ListingModel.findById(req.params.listingid);
+		if(!listing) return res.status(400).send('Listing not found.')
+		return res.status(200).json(listing);
 	}
 	catch (err) {
 		console.error(err);
