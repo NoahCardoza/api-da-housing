c3e0f634ae3ebd6f2b21251948cdfb9249c507be
Carlos Alba <soradev4@gmail.com>
2019-11-02 11:04:44 -0700

    changes

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Routes
 const UserRouter = require('./controllers/User');
+const ListingRouter = require('./controllers/Listing');
 // Application Middlewares
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/', UserRouter)
+app.use('/', UserRouter);
+app.use('/', ListingRouter);
 
 app.get('/', (_, res) => res.send('Index route for API-DA-HOUSING'))
 
