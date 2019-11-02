2bc2d17738d73160594b8abfeec413b56f1ae6b9
Carlos Alba <soradev4@gmail.com>
2019-11-02 14:21:55 -0700

    fmt

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 // Routes
@@ -23,6 +23,6 @@ app.use(cors());
 app.use('/', UserRouter);
 app.use('/', ListingRouter);
 
-app.get('/', (_, res) => res.send('Index route for API-DA-HOUSING'))
+app.get('/', (_, res) => res.send('Index route for API-DA-HOUSING'));
 
-app.listen(process.env.PORT || 3000, () => console.log('service started.'))
+app.listen(process.env.PORT || 3000, () => console.log('service started.'));
