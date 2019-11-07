5daa61da5b8eadea4e24a90fbe28f571b7b0b5de
Carlos Alba <soradev4@gmail.com>
2019-11-06 19:26:42 -0800

    changes to model

--- models/User.js
+++ models/User.js
@@ -25,6 +25,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  favorites: [String],
   tokens: [{
     token: {
       type: String,
@@ -47,9 +48,13 @@ userSchema.pre('save', async function (next) {
 
 userSchema.methods.generateAuthToken = async function () {
   try {
-    const token = jwt.sign({ _id: this._id }, process.env.SECRET);
+    const token = jwt.sign({
+      _id: this._id,
+    }, process.env.SECRET);
     // allows user to be logged in on multiple devices
-    this.tokens = this.tokens.concat({ token });
+    this.tokens = this.tokens.concat({
+      token,
+    });
     await this.save();
     return token;
   } catch (err) {
