a9138acfeb0b3882a90a482fe8ef93a019e9a6ac
Carlos Alba <soradev4@gmail.com>
2019-11-07 17:16:42 -0800

    basic coverage users

--- test/users.test.js
+++ test/users.test.js
@@ -1,8 +1,40 @@
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
 const app = require('../server');
-const { UserModel } = require('../models/User');
+const {
+  UserModel,
+} = require('../models/User');
 
 chai.use(chaiHTTP);
 chai.should();
 
+let jwt = '';
+
+after(async () => {
+  try {
+    console.log('After user tests! deleting users that remain.');
+    await UserModel.findOneAndRemove({
+      email: 'testemail2@gmail.com',
+    }).exec();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+describe('Users', () => {
+  it('Should create user', (done) => {
+    chai.request(app)
+      .post('/create-user')
+      .send({
+        password: 'testpassword123',
+        email: 'testemail2@gmail.com',
+        school: 'De Anza',
+        gender: 'other',
+        name: 'test bot',
+      }).end((err, res) => {
+        if (err) console.error(err);
+        jwt = res.body.token;
+        done();
+      });
+  });
+});
