bd11d2883c8badf60369ea77c4c47a11704f85a5
Carlos Alba <soradev4@gmail.com>
2019-11-23 15:07:35 -0800

    IUser

---
+++ interfaces/index.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+    email?: string;
+    school?: string;
+    gender?: string;
+    password?: string;
+    name?: string;
+    favoriteListings?: string[];
+    preferences?: string[];
+    tokens?: string[];
+}
+
