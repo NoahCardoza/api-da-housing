8414e3e2d21ff63c2a3484d15106c81e8a010010
Carlos Alba <soradev4@gmail.com>
2019-11-24 14:56:16 -0800

    imports

---
+++ src/test/teams.test.ts
@@ -0,0 +1,5 @@
+import chai from "chai";
+import chaiHTTP from "chai-http";
+import UserModel from "../models/User";
+import TeamModel from  "../models/Team";
+import ListingModel from "../models/Listing";
