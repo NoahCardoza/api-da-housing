3118f9bfe7a7ae50bee8ea18624dedf80ac29b48
Carlos Alba <soradev4@gmail.com>
2019-11-24 15:09:50 -0800

    change

--- src/test/teams.test.ts
+++ src/test/teams.test.ts
@@ -65,37 +65,3 @@ after(async () => {
         console.error(err);
     }
 });
-
-//   const TeamSchema: Schema = new mongoose.Schema({
-//     name: {
-//       type: String,
-//       required: true,
-//     },
-//     members: {
-//       type: [mongoose.Schema.Types.ObjectId],
-//       required: true,
-//     },
-//     budget: Number,
-//     favorites: [{
-//       source: {
-//         required: true,
-//         type: [mongoose.Schema.Types.ObjectId],
-//       },
-//       name: {
-//         required: true,
-//         type: String,
-//       },
-//       comments: [String],
-//     }],
-//     outsideFavorites: [{
-//       source: {
-//         required: true,
-//         type: String,
-//       },
-//       name: {
-//         required: true,
-//         type: String,
-//       },
-//       comments: [String],
-//     }],
-//   });
