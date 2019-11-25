2d9fd6cf9f86eac2dab90a250f27c83f6470055d
Carlos Alba <soradev4@gmail.com>
2019-11-25 13:19:07 -0800

    changes

--- src/controllers/Team.ts
+++ src/controllers/Team.ts
@@ -4,6 +4,9 @@ import { auth, ICustomMiddleWareRequest as cm } from "../middleware";
 import TeamModel from "../models/Team";
 import FavoriteModel from "../models/Favorites";
 
+/**
+ * retrieve a team by id.
+ */
 router.get("/team/:id", async (req: cm, res) => {
     try {
         return res.status(200).json(await TeamModel.findById(req.params.id).exec());
