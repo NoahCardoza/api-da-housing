fb65708858a943e249821b14e92b8122bd316c13
Carlos Alba <soradev4@gmail.com>
2019-11-24 17:03:14 -0800

    listing

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,7 @@ mongoose.connect(process.env.MONGO_URI, {
 // Routes
 import ListingRouter from "./controllers/Listing";
 import UserRouter from "./controllers/User";
+import TeamRouter from "./controllers/Listing";
 // Application Middlewares
 app.use(helmet());
 app.use(bodyParser.json());
