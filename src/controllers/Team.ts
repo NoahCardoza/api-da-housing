bca0844f3b76d5da502e6e64e4332bf0891e2a94
Carlos Alba <soradev4@gmail.com>
2019-11-25 13:17:30 -0800

    team id

--- src/controllers/Team.ts
+++ src/controllers/Team.ts
@@ -1,8 +1,16 @@
 import express from "express";
 const router = express.Router();
-import { auth, ICustomMiddleWareRequest  } from "../middleware";
+import { auth, ICustomMiddleWareRequest as cm } from "../middleware";
 import TeamModel from "../models/Team";
 import FavoriteModel from "../models/Favorites";
 
+router.get("/team/:id", async (req: cm, res) => {
+    try {
+        return res.status(200).json(await TeamModel.findById(req.params.id).exec());
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send(error);
+    }
+});
 
 export default router;
