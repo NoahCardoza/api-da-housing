9ac6ac567dfc56c865e6c78e1b94ec94c0a8884b
Carlos Alba <soradev4@gmail.com>
2019-11-25 13:43:22 -0800

    change

--- src/controllers/Team.ts
+++ src/controllers/Team.ts
@@ -41,4 +41,33 @@ router.put("/team/update-team/:id", async (req: cm, res) => {
     }
 });
 
+/**
+ * add a favorite to a team by id.
+ */
+router.put("/team/add-favorite/:id", async (req: cm, res) => {
+    try {
+        const { source, name } = req.body;
+        const favorite = await new FavoriteModel({ source, name }).save();
+        const team = await TeamModel.findById(req.params._id).exec();
+        team.favorites.push(favorite._id);
+        return res.status(204).json(await team.save());
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send(error);
+    }
+});
+
+/**
+ * delete a favorite to a team by id.
+ */
+router.put("/team/add-favorite/:id", async (req: cm, res) => {
+    try {
+        return res.status(204).json()
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send(error);
+    }
+});
+
+
 export default router;
