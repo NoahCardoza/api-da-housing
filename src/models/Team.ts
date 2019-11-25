449ae07cc8e7ac2180ae6ea77070f6735288f1e6
Carlos Alba <soradev4@gmail.com>
2019-11-25 12:52:18 -0800

    changes

--- src/models/Team.ts
+++ src/models/Team.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Model, model, Schema } from "mongoose";
+import FavoriteModel from "../models/Favorites";
 import { ITeam } from "../interfaces";
 
 export interface ITeamModel extends ITeam, Document {
@@ -15,8 +16,8 @@ const TeamSchema: Schema = new mongoose.Schema({
  * Deletes any comments associated or
  * belonging to the team on deletion
  */
-TeamSchema.post("remove", (document) => {
-  
+TeamSchema.post("remove", async (document) => {
+  await FavoriteModel.deleteMany({ team: document._id  }).exec();
 });
 
 const Team: Model<ITeamModel> = model<ITeamModel>("Team", TeamSchema);
