e222b04fd5e977f999ea20a85fc7825061d9ea66
Carlos Alba <soradev4@gmail.com>
2019-11-25 12:45:21 -0800

    changes

--- src/controllers/Team.ts
+++ src/controllers/Team.ts
@@ -3,3 +3,6 @@ const router = express.Router();
 import { auth, ICustomMiddleWareRequest  } from "../middleware";
 import TeamModel from "../models/Team";
 import FavoriteModel from "../models/Favorites";
+
+
+export default router;
