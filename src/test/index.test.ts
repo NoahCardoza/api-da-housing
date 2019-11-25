184faec27bcd54077a132b26a8901d25c2b749ee
Carlos Alba <soradev4@gmail.com>
2019-11-24 18:00:44 -0800

    changes

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -2,3 +2,13 @@
 import * as usersTest from "./users";
 import * as listingsTest from "./listings";
 import * as teamsTest from "./teams";
+
+/**
+ * Forces testing on transpilation
+ * do not delete this.
+ */
+export {
+    usersTest,
+    listingsTest,
+    teamsTest,
+};
