4369942ba190ad09e2ab299bbfda73ae64e69140
Carlos Alba <soradev4@gmail.com>
2019-11-25 13:27:50 -0800

    change

--- src/controllers/Team.ts
+++ src/controllers/Team.ts
@@ -19,7 +19,7 @@ router.get("/team/:id", async (req: cm, res) => {
 router.get("/team/create-team", auth, async (req: cm, res) => {
     try {
         const { name, budget } = req.body;
-        return res.status(201).json(await new TeamModel({name , budget, members: [req.user._id] }).save());
+        return res.status(201).json(await new TeamModel({ name, budget, members: [req.user._id] }).save());
     } catch (error) {
         console.error(error);
         return res.status(500).send(error);
