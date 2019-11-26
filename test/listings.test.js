7464df35cc9bc7ca73d3ee98bf82dcb198b23780
Carlos Alba <soradev4@gmail.com>
2019-11-25 22:27:25 -0800

    changes

--- test/listings.test.js
+++ test/listings.test.js
@@ -2,9 +2,7 @@ const chai = require('chai');
 const chaiHTTP = require('chai-http');
 const app = require('../server');
 const ListingModel = require('../models/Listing');
-const {
-  UserModel,
-} = require('../models/User');
+const UserModel = require('../models/User');
 
 chai.use(chaiHTTP);
 chai.should();
