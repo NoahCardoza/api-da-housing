bb539626ebf0a9abd8a5d34bca12689075d6b15f
Carlos Alba <soradev4@gmail.com>
2019-12-06 18:43:53 -0800

    delete

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 require('dotenv').config();
 
 const app = express();
