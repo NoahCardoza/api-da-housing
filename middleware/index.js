17fa1c61ab03d8e68358d5c4f8f997447e631782
Carlos Alba <soradev4@gmail.com>
2019-12-09 10:28:19 -0800

    smarter middleware

--- middleware/index.js
+++ middleware/index.js
@@ -27,16 +27,19 @@ module.exports.auth = async (req, res, next) => {
 
 module.exports.isListingOwner = async (req, res, next) => {
   try {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    const data = jwt.verify(token, process.env.SECRET);
-    const listingID = req.params.listingid;
-    const listing = await ListingModel.findOne({
-      _id: listingID,
-      author: data._id,
-    }).exec();
-    if (!listing) throw new Error('Credentials failed.');
-    req.listing = listing;
-    return next();
+    if (req.header('Authorization')) {
+      const token = req.header('Authorization').replace('Bearer ', '');
+      const data = jwt.verify(token, process.env.SECRET);
+      const listingID = req.params.listingid;
+      const listing = await ListingModel.findOne({
+        _id: listingID,
+        author: data._id,
+      }).exec();
+      if (!listing) throw new Error('Credentials failed.');
+      req.listing = listing;
+      return next();
+    }
+    throw new Error('Missing Bearer Token for Authorization');
   } catch (err) {
     console.error(err);
     return res.status(500).send('Your credentials have failed the auth layer.');
