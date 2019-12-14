128128a3c9e17b2fb47abbbe1077383da963212a
Carlos Alba <soradev4@gmail.com>
2019-12-13 18:09:17 -0800

    middleware changes

--- middleware/index.js
+++ middleware/index.js
@@ -37,19 +37,16 @@ module.exports.auth = async (req, res, next) => {
 
 module.exports.isListingOwner = async (req, res, next) => {
   try {
-    if (req.header('Authorization')) {
-      const token = req.header('Authorization').replace('Bearer ', '');
-      const data = jwt.verify(token, process.env.SECRET);
-      const listingID = req.params.listingid;
-      const listing = await ListingModel.findOne({
-        _id: listingID,
-        author: data._id,
-      }).exec();
-      if (!listing) throw new Error('Listing not found.');
-      req.listing = listing;
-      return next();
-    }
-    throw new Error('Missing Bearer Token for Authorization');
+    const token = processBearer(req);
+    const data = jwt.verify(token, process.env.SECRET);
+    const listingID = req.params.listingid;
+    const listing = await ListingModel.findOne({
+      _id: listingID,
+      author: data._id,
+    }).exec();
+    if (!listing) throw new Error('Listing not found.');
+    req.listing = listing;
+    return next();
   } catch (error) {
     return res.status(500).send(error.message);
   }
@@ -57,19 +54,16 @@ module.exports.isListingOwner = async (req, res, next) => {
 
 module.exports.isTeamMember = async (req, res, next) => {
   try {
-    if (req.header('Authorization')) {
-      const token = req.header('Authorization').replace('Bearer ', '');
-      const data = jwt.verify(token, process.env.SECRET);
-      const teamID = req.params.teamid;
-      const team = await TeamModel.findOne({
-        _id: teamID,
-        members: data._id,
-      }).exec();
-      if (!team) throw new Error('The Team either does not exist or you are not a member');
-      req.team = team;
-      return next();
-    }
-    throw new Error('Missing Bearer Token for Authorization');
+    const token = processBearer(req);
+    const data = jwt.verify(token, process.env.SECRET);
+    const teamID = req.params.teamid;
+    const team = await TeamModel.findOne({
+      _id: teamID,
+      members: data._id,
+    }).exec();
+    if (!team) throw new Error('The Team either does not exist or you are not a member');
+    req.team = team;
+    return next();
   } catch (error) {
     return res.status(500).send(error.message);
   }
