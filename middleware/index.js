38f9bb7d618885dc46ea92f458cfc06ab5f8f473
Carlos Alba <soradev4@gmail.com>
2019-12-13 18:05:11 -0800

    process bearer

--- middleware/index.js
+++ middleware/index.js
@@ -3,22 +3,32 @@ const UserModel = require('../models/User');
 const ListingModel = require('../models/Listing');
 const TeamModel = require('../models/Team');
 
+/**
+ * @param {*} param0 - request context
+ * takes request contexts and strips
+ * jwt token from Authorization headers.
+ */
+function processBearer({ header }) {
+  const containsBearerToken = header('Authorization') && header('Authorization').includes('Bearer');
+  return containsBearerToken
+    ? header('Authorization').replace('Bearer ', '')
+    : (() => new Error('Either something went wrong when parsing the Bearer Token or it does not exist'))();
+}
+
+
 module.exports.auth = async (req, res, next) => {
   try {
-    if (req.header('Authorization')) {
-      const token = req.header('Authorization').replace('Bearer ', '');
-      const data = jwt.verify(token, process.env.SECRET);
-      const user = await UserModel.findOne({
-        _id: data._id,
-        'tokens.token': token,
-      }).exec();
-      if (!user) throw new Error('Credentials failed.');
-      user.password = undefined;
-      user.tokens = undefined;
-      req.user = user;
-      return next();
-    }
-    throw new Error('Missing Bearer Token for Authorization');
+    const token = processBearer(req);
+    const data = jwt.verify(token, process.env.SECRET);
+    const user = await UserModel.findOne({
+      _id: data._id,
+      'tokens.token': token,
+    }).exec();
+    if (!user) throw new Error('Credentials failed.');
+    user.password = undefined;
+    user.tokens = undefined;
+    req.user = user;
+    return next();
   } catch (error) {
     console.error(error);
     return res.status(500).send(error.message);
