a7e9b1307bef184e91171d7008267db0c3df4852
Carlos Alba <soradev4@gmail.com>
2019-12-16 22:51:58 -0800

    Error Handling Team Controller

--- controllers/Team.js
+++ controllers/Team.js
@@ -13,9 +13,9 @@ router.post('/team/create-team', auth, async (req, res) => {
     const { name, budget } = req.body;
     return res.status(201)
       .json(await new TeamModel({ name, budget, members: [req.user._id] }).save());
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send(error);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send(err.message);
   }
 });
 
@@ -23,9 +23,9 @@ router.post('/team/create-team', auth, async (req, res) => {
 router.get('/team/:id', isTeamMember, async (req, res) => {
   try {
     return res.status(200).json(await TeamModel.findById(req.params.id).exec());
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send(error);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send(err.message);
   }
 });
 
@@ -34,9 +34,9 @@ router.put('/team/update-team/:id', isTeamMember, async (req, res) => {
   try {
     return res.status(204)
       .json(await TeamModel.findByIdAndUpdate(req.params.id, req.body).exec());
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send(error);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send(err.message);
   }
 });
 
@@ -45,9 +45,9 @@ router.put('/team/update-team/:id', isTeamMember, async (req, res) => {
 router.delete('/team/:id', isTeamMember, async (req, res) => {
   try {
     return res.status(202).json(await TeamModel.findByIdAndDelete(req.params.id).exec());
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send(error);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send(err.message);
   }
 });
 
