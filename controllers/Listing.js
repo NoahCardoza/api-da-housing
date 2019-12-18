6e23c10d3520caef6d7d4a33870a5d7432bbefbc
Carlos Alba <soradev4@gmail.com>
2019-12-17 22:24:29 -0800

    Listing Docs

--- controllers/Listing.js
+++ controllers/Listing.js
@@ -41,18 +41,17 @@ router.post('/listing', auth, async (req, res) => {
   }
 });
 
-/** Read Route for Listing resource */
 /**
  * @swagger
  * /listing:
  *  get:
- *    description: Read Route for Favorite resource.
+ *    description: Read Route for Listing resource.
  *    produces:
  *    - "application/json"
  *    parameters:
  *    - in: path
  *      name: id
- *      description: The id of a given Favorite.
+ *      description: The id of a given Listing.
  *    responses:
  *      '200':
  *            description: A json object representing a Listing or a List of Listings.
@@ -73,7 +72,27 @@ router.get('/listing', async (req, res) => {
   }
 });
 
-/** Update Route for Listing resource */
+/**
+ * @swagger
+ * /listing:
+ *  put:
+ *    description: Update Route for Listing resource.
+ *    produces:
+ *    - "application/json"
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      description: The id of a given Listing.
+ *      required: true
+ *    - in: body
+ *      name: body
+ *      description: an object containing the fields that need updating
+ *    responses:
+ *      '200':
+ *            description: A json object representing a Listing.
+ *      '500':
+ *            description: server error
+ */
 router.put('/listing/:listingid', isListingOwner, async (req, res) => {
   try {
     const listingID = req.listing._id;
@@ -88,7 +107,24 @@ router.put('/listing/:listingid', isListingOwner, async (req, res) => {
   }
 });
 
-/** Delete Route for Listing resource */
+/**
+ * @swagger
+ * /listing:
+ *  delete:
+ *    description: Delete Route for Listing resource.
+ *    produces:
+ *    - "application/json"
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      description: The id of a given Listing.
+ *      required: true
+ *    responses:
+ *      '202':
+ *            description: successfully deleted
+ *      '500':
+ *            description: server error
+ */
 router.delete('/listing/:listingid', isListingOwner, async (req, res) => {
   try {
     // passed in by isListingOwner Middleware.
