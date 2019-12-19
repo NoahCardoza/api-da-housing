31220b641c1aede9c42ce1e8397e5b579fc3c6d0
Carlos Alba <soradev4@gmail.com>
2019-12-18 22:46:16 -0800

    Org Controller

---
+++ controllers/Organization.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const router = express.Router();
+const { auth } = require('../middleware');
+const OrganizationModel = require('../models/Organization');
+
+/**
+ * @swagger
+ * /organization:
+ *  post:
+ *    description: Create Route for organization resource.
+ *    produces:
+ *    - "application/json"
+ *    parameters:
+ *    - in: "body"
+ *      name: "body"
+ *      description: A object containing name, location, members and administrators
+ *    examples:
+ *      application/json: {"name":"De Anza",location:"Cupertino",members:[],administrators:[]}
+ *    responses:
+ *      '201':
+ *            description: An object containing a Organization
+ *      '500':
+ *            description: server error
+ */
+router.post('/organization', auth, async (req, res) => {
+  try {
+    const organization = new OrganizationModel(req.body);
+    await organization.save();
+    return res.status(201).json(organization);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send(err.message);
+  }
+});
+
+module.exports = router;
