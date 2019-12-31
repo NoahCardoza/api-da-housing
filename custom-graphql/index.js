e20fc79293e9db053ea93eed47f9834f916cdeba
Carlos Alba <soradev4@gmail.com>
2019-12-30 22:10:47 -0800

    login resolver

--- custom-graphql/index.js
+++ custom-graphql/index.js
@@ -1,10 +1,27 @@
-const { ApolloServer, gql } = require('apollo-server-express');
+const {
+  ApolloServer,
+  gql,
+} = require('apollo-server-express');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const typeDefs = gql`
+  type Address {
+    street: String
+    city: String
+    zipcode: Int
+  }
+  type Listing {
+    _id: ID
+    price: Float
+    images: [String]
+    description: String
+    author: ID
+    address: Address
+  }
   type User {
-    profilePicture: String, 
+    _id: ID
+    profilePicture: String
     personalGallery: [String]
     email: String
     organization: String
@@ -15,14 +32,62 @@ const typeDefs = gql`
   }
   type Query {
     user: User
+    login(email: String!, password: String!): String
+  }
+  type Mutation {
+    user(profilePicture: String, personalGallery: [String],
+    email: String,
+    organization: String,
+    gender: String,
+    name: String,
+    favoriteListings: [ID],
+    preferences: [String], password: String): User
   }
 `;
 
 const resolvers = {
   Query: {
     user: async (parent, args, context) => {
-      if (!context.user) return null;
-      return context.user;
+      try {
+        if (!context.user) return null;
+        if (!context.user.profilePicture) {
+          return {
+            ...context.user._doc,
+            profilePicture: 'https://www.pphfoundation.ca/wp-content/uploads/2018/05/default-avatar.png',
+          };
+        }
+        return context.user;
+      } catch (error) {
+        console.log(error.message);
+        return error.message;
+      }
+    },
+    login: async (parent, args, context) => {
+      try {
+        const user = await User.findOne({
+          email: args.email,
+        });
+        if (user === null) throw new Error('User not found');
+        const result = await user.comparePassword(args.password);
+        if (result === true) return user.generateAuthToken();
+        throw new Error('Credentials Have Failed');
+      } catch (error) {
+        console.log(error.message);
+        return error.message;
+      }
+    },
+  },
+  Mutation: {
+    user: async (parent, args, context) => {
+      try {
+        if (!context.user) {
+          return new User(args).save();
+        }
+        return User.findByIdAndUpdate(context.user._id, args).exec();
+      } catch (error) {
+        console.log(error.message);
+        return error.message;
+      }
     },
   },
 };
@@ -30,12 +95,16 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({ req }) => {
+  context: async ({
+    req,
+  }) => {
     try {
       const token = (req.headers.authorization || '').replace('Bearer ', '');
       const data = jwt.verify(token, process.env.SECRET);
-      const user = await User.findOne({ _id: data._id, 'tokens.token': token }).exec();
-      console.log(user);
+      const user = await User.findOne({
+        _id: data._id,
+        'tokens.token': token,
+      }).exec();
       return {
         user,
       };
