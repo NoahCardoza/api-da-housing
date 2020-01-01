0ecac12163ed98bfa1a87a43ed7a000daf5e4593
Carlos Alba <soradev4@gmail.com>
2020-01-01 14:46:09 -0800

    token auth middleware

--- custom-graphql/index.js
+++ custom-graphql/index.js
@@ -194,29 +194,26 @@ const resolvers = {
   },
 };
 
+const tokenAuthorizationMiddleware = async ({ req }) => {
+  try {
+    const TOKEN = (req.headers.authorization || '').replace('Bearer ', '');
+    const DATA = Object.freeze(jwt.verify(TOKEN, process.env.SECRET));
+    const user = await User.findOne({ _id: DATA._id, 'tokens.token': TOKEN }).exec();
+    return {
+      user,
+    };
+  } catch (error) {
+    console.log(error.message);
+    return {
+      user: null,
+    };
+  }
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({
-    req,
-  }) => {
-    try {
-      const token = (req.headers.authorization || '').replace('Bearer ', '');
-      const data = jwt.verify(token, process.env.SECRET);
-      const user = await User.findOne({
-        _id: data._id,
-        'tokens.token': token,
-      }).exec();
-      return {
-        user,
-      };
-    } catch (error) {
-      console.log(error.message);
-      return {
-        user: null,
-      };
-    }
-  },
+  context: tokenAuthorizationMiddleware,
 });
 
 module.exports = {
