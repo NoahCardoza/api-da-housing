d82491c77bd45ffd90e3457787b8f6335991b01d
Carlos Alba <soradev4@gmail.com>
2020-01-01 16:19:00 -0800

    formatting

--- models/Team.js
+++ models/Team.js
@@ -13,16 +13,14 @@ const TeamSchema = new mongoose.Schema({
   favorites: [mongoose.Schema.Types.ObjectId],
 });
 
-
 /**
  * Delete Teams with no members.
  */
-TeamSchema.post('find', async function (docs) {
+TeamSchema.post('find', async function(docs) {
   const teamMembers = this.members;
   if (Array.isArray(teamMembers) && teamMembers.length) {
     this.remove();
   }
 });
-
 
 module.exports = mongoose.model('team', TeamSchema);
